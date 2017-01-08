ce3087b82259897bf0eec3fa0449b9f6d2efe16a
madman61rus <rhjnhjtn3yjhe>
2017-01-08 14:18:17 +0300

    - Перименовал некоторые переменные + Вынес похожие проверки в одну функцию + Поправил код в соответсвие с PEP8 + Убрал лишние комментарии

--- password_strength.py
+++ password_strength.py
@@ -3,7 +3,6 @@ import os
 import re
 
 def check_blacklist(file_path, password):
-    '''Проверка на присутствие пароля в черном списке'''
     if not os.path.exists(file_path):
         return None
     with open(file_path, 'r', encoding='utf-8') as data_file:
@@ -14,69 +13,56 @@ def check_blacklist(file_path, password):
         else:
             return 0,None
 
-def check_password_length(password):
-    '''Проверка длины пароля'''
-    if len(password) < 6 :
+def check_password_length(password,length=6):
+    if len(password) < length :
         return 1 , 'Длина пароля должна быть больше 6 символов'
     else :
         return 0,None
 
 def check_сase_complication(password):
-    '''Проверка на содержание больших и маленьких букв'''
     if not any(ch.isupper() for ch in password) and any(ch.islower() for ch in password):
         return 1, 'Пароль должен содержать символы в верхнем и нижнем регистрах'
     else:
         return 0,None
 
 def check_digits(password):
-    '''Проверка на присутсвие цифр в пароле'''
     if not any(ch.isdigit() for ch in password):
         return 1, 'Пароль должен содержать хотя бы одну цифру'
     else:
         return 0,None
 
-def check_special_characters(password):
-    '''Проверка на присутствие специальных символов'''
-    pattern = re.compile(r'[$*#!^%_?]')
-    if not re.findall(pattern,password):
-        return 1, 'Пароль должен содержать специальные символы (#,!,^)'
+def check_regular_expression(password,pattern,message):
+    if re.search(pattern,password):
+        return 1, message
     else:
-        return 0,None
-
-def check_email(password):
-    '''Проверка на присутствие email в пароле'''
-    if re.search(r'[\w\.-]+@[\w\.-]+',password):
-        return 1,'Пароль не должен содержать email'
-    else:
-        return 0,None
-
-def check_phone_number(password):
-    '''Проверка на присутствие номеров телефонов в пароле'''
-    if re.search(r'[\dA-Z]{3}-[\dA-Z]{2}-[\dA-Z]{2}',password) or re.search(r'[\dA-Z]{3}-[\dA-Z]{7}',password):
-        return 1, 'Пароль не должен содержать номера телефонов'
-    else:
-        return 0,None
+        return 0, None
 
 
-def get_password_strength(password,file_path):
-    '''Функция вычисления сложности'''
 
-    strength = 10
+def get_password_strength(password,file_path,strength=10):
+
     message = []
 
-    tmp = check_blacklist(file_path,password)
-    strength -= tmp[0]
-    message.append(tmp[1])
 
-    functions = [check_password_length,check_сase_complication,check_digits,check_special_characters ,check_email,check_phone_number,check_phone_number]
+    functions = [check_blacklist(file_path,password),check_password_length(password),check_сase_complication(password),
+                 check_regular_expression(password,r'[^$*#!^%_?]',
+                                          'Пароль должен содержать специальные символы (#,!,^)'),
+                 check_regular_expression(password, r'[\w\.-]+@[\w\.-]+','Пароль не должен содержать email'),
+                 check_regular_expression(password, r'[\dA-Z]{3}-[\dA-Z]{2}-[\dA-Z]{2}',
+                                          'Пароль не должен содержать номера телефонов'),
+                 check_regular_expression(password, r'[\dA-Z]{3}-[\dA-Z]{7}',
+                                          'Пароль не должен содержать номера телефонов')
+                 ]
 
-    for func in functions:
-        tmp = func(password)
-        strength -= tmp[0]
-        if tmp[1]:
-            message.append(tmp[1])
+    for function in functions:
+        check_result = function
+        strength -= check_result[0]
+        if check_result[0]:
+            message.append(check_result[1])
 
-    return strength,message
+
+
+    return strength, message
 
 
 if __name__ == '__main__':
